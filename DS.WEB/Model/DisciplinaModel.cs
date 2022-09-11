44b03c4f06ced669054a8b6d81c96f94fe2f7fce
lucasoliveirasouza <lycasoliveira@gmail.com>
2022-09-11 20:36:09 -0300

    Atualizando telas

--- DS.WEB/Model/DisciplinaModel.cs
+++ DS.WEB/Model/DisciplinaModel.cs
@@ -4,6 +4,7 @@
     {
         public string Nome { get;set;}
         public string CargaHoraria { get; set; }
+        public string Horario { get; set; }
         public string Id { get; set; }
 
     }
